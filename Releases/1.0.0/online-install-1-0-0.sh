2f7e2b2e4a403615ccd7f1b9174435ecbd648435
bradeythedev <maybp77@yahoo.com>
2022-01-27 18:13:18 +0800

    maybe fix this time?

--- Releases/1.0.0/online-install-1-0-0.sh
+++ Releases/1.0.0/online-install-1-0-0.sh
@@ -1,5 +1,9 @@
-git clone "https://github.com/bradeythedev/cowfetch/" "cowfetch-stable-1-0-0"
-cd "cowfetch-stable-1-0-0"
+git clone "https://github.com/bradeythedev/cowfetch/"
+cd "cowfetch"
+cd "Releases"
+cd "1.0.0"
 sudo make install
 cd ..
-rm -rf "cowfetch-stable-1-0-0"
+cd ..
+cd ..
+rm -rf "cowfetch"
