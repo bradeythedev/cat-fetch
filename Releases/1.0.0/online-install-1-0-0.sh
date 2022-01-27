b32164bf62ca3241d2f98d4e129ccb0d23feecc3
bradeythedev <maybp77@yahoo.com>
2022-01-27 18:09:41 +0800

    fix online install (i hope)

--- Releases/1.0.0/online-install-1-0-0.sh
+++ Releases/1.0.0/online-install-1-0-0.sh
@@ -1,7 +1,5 @@
-mkdir "cowfetch-stable-1-0-0"
+git clone "https://github.com/bradeythedev/cowfetch/" "cowfetch-stable-1-0-0"
 cd "cowfetch-stable-1-0-0"
-git clone "https://github.com/bradeythedev/cowfetch/blob/f9c3acacfa5cdaae48ed337ed21bb7732effa4d1/Releases/1.0.0/cowfetch"
-git clone "https://github.com/bradeythedev/cowfetch/blob/f9c3acacfa5cdaae48ed337ed21bb7732effa4d1/Releases/1.0.0/Makefile"
 sudo make install
 cd ..
 rm -rf "cowfetch-stable-1-0-0"
